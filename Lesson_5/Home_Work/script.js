6d1d94b4db86a62ded77e5e1c3753c7233e578ea
SaniaGos <saniagoss@gmail.com>
2021-11-27 11:19:49 +0200

    js_lesson_5_hw_done

--- Lesson_5/Home_Work/script.js
+++ Lesson_5/Home_Work/script.js
@@ -1,68 +1,78 @@
-// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-function squareRectangle(a, b) {
-    return a * b;
-}
+// Всі функції повинні бути описані стрілочним типом!!!!
+// - створити функцію яка обчислює та повертає площу прямокутника висотою
+const areaRectangle = (a, b) => a * b;
 
-// - створити функцію яка обчислює та повертає площу кола з радіусом r
-function squareRound(r) {
-    return Math.PI * r ** 2
-}
+let a = 5;
+let b = 4;
+console.log("Площа прямокутнику з сторонами " + a + " і " + b + " = " + areaRectangle(a, b));
+document.write("Площа прямокутнику з сторонами " + a + " і " + b + " = " + areaRectangle(a, b) + '<br>');
 
-// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-function squareCylinder(h, r) {
-    return squareRound(r) * 2 + (2 * Math.PI * r * h);  // взяв в дужки для зрозумілості
-}
+// - створити функцію яка обчислює та повертає площу кола
+const areaCircle = (r) => Math.round(Math.PI * r ** 2 * 100) / 100;
+
+let r = 2;
+console.log("Площа кола з радіусом " + r + " = " + areaCircle(r));
+document.write("Площа кола з радіусом " + r + " = " + areaCircle(r) + '<br>');
+
+// - створити функцію яка обчислює та повертає площу циліндру
+const areaCylinder = (r, h) => areaCircle(r) * h;
+
+let h = 5;
+console.log("Площа кола з радіусом " + r + " і висотою " + h + " = " + areaCylinder(r, h));
+document.write("Площа кола з радіусом " + r + ' і висотою ' + h + " = " + areaCylinder(r, h) + '<br>');
 
 // - створити функцію яка приймає масив та виводить кожен його елемент
-function logArray(arr) {
-    for (let element of arr) {
-        document.writeln(element);
-        console.log(element);
+const arrWrite = (arr) => {
+    if (Array.isArray(arr)) {
+        arr.forEach(el => {
+            console.log(el);
+            document.write(el + '<br>');
+        })
     }
 }
+arrWrite([5, 'hello', true, 456]);
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-function createParagraph(text) {
-    document.write(`<p>${text}</p>`)
+const textInTeg_p = (...text) => {
+    text.forEach(text => {
+        document.write(`<p>${text}</p>`)
+    });
 }
+textInTeg_p('Hello', 'okten', 555);
 
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-function createUl3Li(text) {
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+const textInTeg_li = (text) => {
     document.write('<ul>');
-    for (let i = 0; i < 3; i++) document.write(`<li>${text}</li>`);
+    for (let i = 0; i < 3; i++) {
+        document.write(`<li>${i + 1} ${text}</li>`)
+    }
     document.write('</ul>');
 }
+textInTeg_li('List')
 
-// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-function createUl(text, numOfLi) {
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+const textInTeg_liDynamic = (text, nulOfLi = 0) => {
     document.write('<ul>');
-    for (let i = 0; i < numOfLi; i++) document.write(`<li>${text}</li>`);
+    for (let i = 0; i < nulOfLi; i++) {
+        document.write(`<li>${i + 1} ${text}</li>`)
+    }
     document.write('</ul>');
 }
+textInTeg_liDynamic('List', 5);
 
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-function createUlFromArr(arr) {
+const primitivesInTeg_li = (arrOfPrimitives) => {
+    if (!Array.isArray(arrOfPrimitives)) return;
     document.write('<ul>');
-    if (Array.isArray(arr)) arr.forEach(element => {                // перевіряєм чи вхідні дані є масивом
-        document.write(`<li>${element}</li>`)
-    })
+    arrOfPrimitives.forEach( primitive => document.write(`<li>${primitive}</li>`))
     document.write('</ul>');
 }
-
+primitivesInTeg_li(['Hello', 555, true]);
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-function writeObjectFromArr(arrOfObject) {
-    if (Array.isArray(arrOfObject)) arrOfObject.forEach(element => {
-    document.write('<div>');
-        document.write(`<p>Id: <b>${element.id}</b> </p>`);
-        document.write(`<p>Name: <b>${element.name}</b> </p>`);
-        document.write(`<p>Age: <b>${element.age}</b> </p>`);
-    document.write('</div>');
+const showObject = (arrayOfObject) => {
+    if (!Array.isArray(arrayOfObject)) return;
+    arrayOfObject.forEach( obj => {
+        document.write(`<div><b>id</b> - ${obj.id}, <b>ім'я</b> - ${obj.name}, <b>вік</b> - ${obj.age} </div>`)
     })
 }
-
-writeObjectFromArr(
-    [{id: 1, name: 'vasya', age: 31},
-    {id: 2, name: 'petya', age: 30},
-    {id: 3, name: 'kolya', age: 29},
-    {id: 4, name: 'olya', age: 28}]
-);
+showObject([{id: 1, name: 'Oleksandr', age: 33},{id: 2, name: 'Vita', age: 25},{id: 3, name: 'Oleh', age: 30}]);
